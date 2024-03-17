5e4dcdb8620c7b7cea447fbe7c73b23e7277b15e
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-03-17 18:50:48 +0530

    Pointer event functions are added

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -3,16 +3,43 @@ btnstop=document.querySelector('#stop')
 btnreset=document.querySelector('#reset')
 
 let hr=min=sec=ms=0;
+
 btnstart.addEventListener('click',()=>{
 
-})
+    setTimer=setInterval(()=>{
+    btnstart.classList.add('start-active');
+    btnstop.classList.remove('stop-active');
+    ms++;
+    if (ms==100){
+        sec++;
+        ms=0;
+    }
+    if (sec==60){
+        min++;
+        sec=0;
+    }
+    if (min==60){
+        hr++;
+        min=0;
+    }
+    Updatedisplay();
+},10);
+});
+
 btnstop.addEventListener('click',()=>{
-    
-})
+
+    btnstart.classList.remove('start-active');
+    btnstop.classList.add('stop-active');
+clearInterval(setTimer);
+});
+
 btnreset.addEventListener('click',()=>{
+    
+    btnstart.classList.remove('start-active');
+    btnstop.classList.remove('stop-active');
     hr=min=sec=ms=0;
     Updatedisplay();
-})
+});
 
 function Updatedisplay(){
     phr=hr<10?'0'+hr:hr;
